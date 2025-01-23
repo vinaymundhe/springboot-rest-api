609db9d3e5dba3acb7dd73d59dce9f6ffbc98f1a
Vinay Mundhe <vinay.mundhe19@gmail.com>
2025-01-23 22:58:04 +0530

    HTTP Post Request

--- src/main/java/com/flygenmedia/springboot/controller/StudentController.java
+++ src/main/java/com/flygenmedia/springboot/controller/StudentController.java
@@ -1,10 +1,8 @@
 package com.flygenmedia.springboot.controller;
 
 import com.flygenmedia.springboot.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,5 +49,16 @@ public class StudentController {
                                           @RequestParam String firstName,
                                           @RequestParam String lastName){
         return new Student(id, firstName, lastName);
+    }
+
+//    Spring boot REST API that handles HTTP POST Request
+//    http://localhost:8080/students/create
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student){
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 }
